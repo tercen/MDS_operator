045ba53877fe63e10b7efc4b865eb3ddbe042de8
agouy <contact@agouy.com>
2020-06-22 20:59:48 +0000

    first commit

--- main.R
+++ main.R
@@ -1,9 +1,28 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+do.mds <- function(df) {
+  
+  dist.mat <- as.dist(df)
+  
+  out <- data.frame(
+    .ci = seq_len(dim(df)[1]) - 1,
+    mds_1 = NaN,
+    mds_2 = NaN
+  )
+  
+  fit <- cmdscale(dist.mat, eig = TRUE, k = 2)
+  
+  out$mds_1 <- fit$points[,1]
+  out$mds_2 <- fit$points[,2]
+  
+  return(out)
+}
+
+(ctx = tercenCtx())  %>%
+  select(.y, .ci, .ri) %>%
+  reshape2::acast(.ci ~ .ri, value.var = '.y', fun.aggregate = mean) %>%
+  do.mds() %>%
+  as_tibble() %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,33 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "d945b82176ce8b251b3ab7bfde057cd4")
+options("tercen.stepId"     = "63b37844-be38-4638-b0b4-8ec111cde929")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+do.mds <- function(df) {
+  
+  dist.mat <- as.dist(df)
+  
+  out <- data.frame(
+    .ci = seq_len(dim(df)[1]) - 1,
+    mds_1 = NaN,
+    mds_2 = NaN
+  )
+  
+  fit <- cmdscale(dist.mat, eig = TRUE, k = 2)
+
+  out$mds_1 <- fit$points[,1]
+  out$mds_2 <- fit$points[,2]
+
+  return(out)
+}
+
+(ctx = tercenCtx())  %>%
+  select(.y, .ci, .ri) %>%
+  reshape2::acast(.ci ~ .ri, value.var='.y', fun.aggregate=mean) %>%
+  do.mds() %>%
   ctx$addNamespace() %>%
   ctx$save()
